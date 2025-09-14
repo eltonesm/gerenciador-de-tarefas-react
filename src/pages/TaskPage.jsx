79abd16e903a8bb82d2fef97009f211f631adfb2
eltonesm <eltonesm@gmail.com>
2025-09-13 22:31:17 -0300

    feat: correcao tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,10 +7,9 @@ function TaskPage() {
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
-
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <div>
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
             onClick={() => navigate(-1)}
@@ -18,10 +17,11 @@ function TaskPage() {
           >
             <ChevronLeftIcon />
           </button>
-          <Title>Detalhe da Tarefa</Title>
+          <Title>Detalhes da Tarefa</Title>
         </div>
+
         <div className="bg-slate-200 p-4 rounded-md">
-          <h2 className="text-xl text-slate-600 font-bold">{title}</h2>
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
           <p className="text-slate-600">{description}</p>
         </div>
       </div>
