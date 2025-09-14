284d4716bb15342c9d2678edf2ff3d1f3d313293
eltonesm <eltonesm@gmail.com>
2025-09-13 22:12:43 -0300

    feat: corrigi import

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import Input from "./input";
+import Input from "./Input";
 
 function AddTask({ onAddTaskSubmit }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <Input
@@ -12,6 +13,7 @@ function AddTask({ onAddTaskSubmit }) {
         value={title}
         onChange={(event) => setTitle(event.target.value)}
       />
+
       <Input
         type="text"
         placeholder="Digite a descrição da tarefa"
@@ -20,6 +22,7 @@ function AddTask({ onAddTaskSubmit }) {
       />
       <button
         onClick={() => {
+          // verificar se o título e a descrição estão preenchidos
           if (!title.trim() || !description.trim()) {
             return alert("Preencha o título e a descrição da tarefa.");
           }
@@ -27,7 +30,7 @@ function AddTask({ onAddTaskSubmit }) {
           setTitle("");
           setDescription("");
         }}
-        className="bg-slate-500 text-white px-4 py-2 rousnded-md font-medium"
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
       >
         Adicionar
       </button>
